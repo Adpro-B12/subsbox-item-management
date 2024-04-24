89d239ee507da81bd010366fca0450f323f7b5c3
Clarence Grady <clarence.grady@ui.ac.id>
2024-04-24 19:26:28 +0700

    [GREEN] make implementation for subscription box model

--- src/main/java/id/ac/ui/cs/advprog/subsbox_item_management/model/SubscriptionBox.java
+++ src/main/java/id/ac/ui/cs/advprog/subsbox_item_management/model/SubscriptionBox.java
@@ -2,20 +2,18 @@ package id.ac.ui.cs.advprog.subsbox_item_management.model;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.UUID;
 import java.util.List;
 
-@Builder
-@Getter
+@Getter @Setter
 public class SubscriptionBox {
     String id;
     String name;
     String type;
     int price;
     List<Item> items;
-
-    public SubscriptionBox(String name, String type, int price, List<Item> items) {
-        
-    }
+    // Rating rating;
 }
---
+++ src/main/java/id/ac/ui/cs/advprog/subsbox_item_management/model/builder/SubscriptionBoxBuilder.java
@@ -0,0 +1,45 @@
+package id.ac.ui.cs.advprog.subsbox_item_management.model.builder;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import id.ac.ui.cs.advprog.subsbox_item_management.model.SubscriptionBox;
+import id.ac.ui.cs.advprog.subsbox_item_management.repository.SubscriptionBoxRepository;
+
+public class SubscriptionBoxBuilder {
+    private SubscriptionBox subscriptionBox;
+
+    @Autowired
+    private SubscriptionBoxRepository subscriptionBoxRepository;
+    
+    public SubscriptionBoxBuilder() {
+        this.subscriptionBox = new SubscriptionBox();
+    }
+
+    public SubscriptionBoxBuilder setName(String name) {
+        subscriptionBox.setName(name);
+        return this;
+    }
+
+    public SubscriptionBoxBuilder setType(String type) {
+        subscriptionBox.setType(type);
+        return this;
+    }
+
+    public SubscriptionBoxBuilder setPrice(int price) {
+        subscriptionBox.setPrice(price);
+        return this;
+    }
+
+    public SubscriptionBoxBuilder setItems() {
+        subscriptionBox.setItems(new ArrayList<>());
+        return this;
+    }
+
+    public SubscriptionBoxBuilder setId(String id) {
+        subscriptionBox.setId(id);
+        return this;
+    }
+
+}
