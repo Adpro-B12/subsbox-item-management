3fcfb8fd0ab1565745e440b6421bee50ba8b42b4
Clarence Grady <clarence.grady@ui.ac.id>
2024-05-24 18:40:32 +0700

    fix import

--- src/main/java/id/ac/ui/cs/advprog/subsbox_item_management/model/builder/SubscriptionBoxBuilder.java
+++ src/main/java/id/ac/ui/cs/advprog/subsbox_item_management/model/builder/SubscriptionBoxBuilder.java
@@ -1,11 +1,9 @@
 package id.ac.ui.cs.advprog.subsbox_item_management.model.builder;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import id.ac.ui.cs.advprog.subsbox_item_management.model.SubscriptionBox;
-import id.ac.ui.cs.advprog.subsbox_item_management.repository.SubscriptionBoxRepository;
 
 
 @Component
