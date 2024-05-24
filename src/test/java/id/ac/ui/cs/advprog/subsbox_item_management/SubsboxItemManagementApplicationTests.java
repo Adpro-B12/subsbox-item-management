51f179eb3c9fbbe7870e553b2323a24453e3a91c
alifbintang1 <alifbintange@gmail.com>
2024-05-24 21:21:55 +0700

    solve properties

--- src/test/java/id/ac/ui/cs/advprog/subsbox_item_management/SubsboxItemManagementApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/subsbox_item_management/SubsboxItemManagementApplicationTests.java
@@ -2,6 +2,9 @@ package id.ac.ui.cs.advprog.subsbox_item_management;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
 
 @SpringBootTest
 class SubsboxItemManagementApplicationTests {
